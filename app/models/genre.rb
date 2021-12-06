351c81174e64dc2ff612698e85220b35b0361cf5
yusukexyusuke <yusukexyusuke@gmail.com>
2021-12-06 20:53:33 +0900

    グルーピングされた値を返すクラスメソッド作成

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -6,6 +6,40 @@ class Genre < ApplicationRecord
   AGENT = Mechanize.new
   BASE_URL = 'https://yomikatawa.com/kanji/'
 
+  def self.grouping_genres
+    Genre.all.group_by do |genre|
+      name = genre.name
+      if genre.name.match(/[一-龠々]/)
+        name = AGENT.get(BASE_URL + genre.name).search('#content p').first.inner_text
+      end
+
+      case name[0]
+      when /[ぁ-お]/
+        'あ'
+      when /[か-ご]/
+        'か'
+      when /[さ-ぞ]/
+        'さ'
+      when /[た-ど]/
+        'た'
+      when /[な-の]/
+        'な'
+      when /[は-ぽ]/
+        'は'
+      when /[ま-も]/
+        'ま'
+      when /[ゃ-よ]/
+        'や'
+      when /[ら-ろ]/
+        'ら'
+      when /[ゎ-を]/
+        'わ'
+      else
+        name[0]
+      end
+    end
+  end
+
   def to_hiragana(kanji)
     AGENT.get(BASE_URL + kanji).search('#content p').first.inner_text
   end
