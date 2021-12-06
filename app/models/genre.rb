b1a1c97b97950adeac20a0d4d51a7260a8957c50
Ryo-0912 <tensaisander0912@gmail.com>
2021-12-06 15:54:46 +0900

    言葉の表記で場合分け #8

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -20,4 +20,36 @@ class Genre < ApplicationRecord
     AGENT.get(BASE_URL + kanji).search('.alert').empty?
   end
 
+  def kanji_to_kana
+    genres = %w[あかがい うなぎ ほたて]
+
+    kana = genres.group_by do |item|
+      case item[0]
+      when /[ぁ-お]/
+        'あ'
+      when /[か-ご]/
+        'か'
+      when /[さ-ぞ]/
+        'さ'
+      when /[た-ど]/
+        'た'
+      when /[な-の]/
+        'な'
+      when /[は-ぽ]/
+        'は'
+      when /[ま-も]/
+        'ま'
+      when /[ゃ-よ]/
+        'や'
+      when /[ら-ろ]/
+        'ら'
+      when /[ゎ-を]/
+        'わ'
+      else
+        item[0]
+      end
+    end
+
+  end
+
 end
