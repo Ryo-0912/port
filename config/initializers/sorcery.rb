2c2f828367b4e58e94c466c1259397602a2fd595
Ryo-0912 <tensaisander0912@gmail.com>
2022-01-21 14:35:27 +0900

    ログイン状態とカウントされる件 #30

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -1,14 +1,16 @@
-
+Rails.application.config.sorcery.submodules = [:reset_password, :remember_me]
 Rails.application.config.sorcery.submodules = [:session_timeout]
 
 # Here you can configure each submodule's features.
 Rails.application.config.sorcery.configure do |config|
+  config.session_timeout = 300
+  config.session_timeout_from_last_action = true
 
   config.user_config do |user|
     user.reset_password_mailer = UserMailer
     user.reset_password_email_method_name = :reset_password_email
-    config.session_timeout = 180
-    config.session_timeout_from_last_action = true
+    user.remember_me_for = 180
+    user.remember_me_token_persist_globally = true
   end
 
   # This line must come after the 'user config' block.
