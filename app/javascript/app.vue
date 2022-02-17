03542fd3f66f3087a82eec82e032f0fec178972a
Ryo-0912 <tensaisander0912@gmail.com>
2022-02-17 19:12:09 +0900

    ジャンルの詳細画面に遷移 #34

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     {{id}}
+    <span v-if="glink">
+      <span @click="genrelink">{{name}}</span>
+    </span>
     <i id="pen" class="fas fa-pen" @click="editgg"></i>
-    <form v-if="edit">
-      <label for="name" hidden></label>
-      <input type="text" v-model="name" name="genre[name]" value="">
-      <button @click="save">登録</button>
-    </form>
+    <span class="egform">
+      <form v-if="edit">
+        <label for="name" hidden></label>
+        <input class="edgform" type="text" v-model="name">
+          <button class="cancel-btn" @click="cancel">キャンセル</button><button class="save-btn" @click.prevent="save">登録</button>
+      </form>
+    </span>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+
 
 axios.defaults.headers.common = {
     'X-Requested-With': 'XMLHttpRequest',
@@ -19,34 +25,56 @@ axios.defaults.headers.common = {
 };
 
 export default {
-  props: ['id'],
+  props: ['id', 'name'],
   data: function () {
     return {
+      glink: true,
       edit: false,
-      name: ''
+      counter: this.id,
+      counter: this.name
     }
   },
   mounted(){
     console.log('aaaaa')
   },
   methods: {
+    genrelink: function() {
+      axios.get(`/genres/${this.id}/questions`)
+            .then(res => {
+              console.log("遷移しました")
+            });
+    },
     editgg: function() {
-      this.edit = true
+      this.glink = false,
+      this.edit = true,
+      this.name = `${this.name}`
     },
     save: function() {
-      axios.patch(`/genres/${this.id}`)
+      axios.patch(`/genres/${this.id}`, { genre: { name: this.name } })
             .then(res => {
               console.log("ジャンルを登録しました")
-              this.name = res.data;
             });
+    },
+    cancel: function() {
+      this.glink = true,
+      this.edit = false
     }
   }
 }
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+<style>
+.edgform{
+  margin-left: -10px;
+  border-radius: 5px;
+  margin-bottom: 2px;
+}
+.cancel-btn{
+  margin-left: 22.3px;
+  border-radius: 5px;
+}
+.save-btn{
+  margin-left: 2px;
+  border-radius: 5px;
 }
 </style>
