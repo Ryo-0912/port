fac3efcf4a04f5764bff39b16fda48722ba792c8
Ryo-0912 <tensaisander0912@gmail.com>
2022-02-25 10:17:37 +0900

    original_urlについて #41

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'My Note',
+      title: 'My Note',
+      reverse: true,
+      charset: 'utf-8',
+      description: '',
+      keywords: '勉強,暗記,苦手',
+      canonical: request.original_url,
+      separator: '|', # これで"タイトル | サイトネーム"ってなる
+      icon: [
+        { href: image_url('logo.png') }
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        local: 'ja-JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@あなたのツイッターアカウント'
+      }
+    }
+  end
 end
