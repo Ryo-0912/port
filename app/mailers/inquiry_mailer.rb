63d71ae5cd651318605729e8f6417d96331f8393
Ryo-0912 <tensaisander0912@gmail.com>
2022-03-28 12:11:10 +0900

    メールに関する設定

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,5 +1,5 @@
 class InquiryMailer < ApplicationMailer
-  default to: "mynote20220201@example.com"
+  default to: "mynote20220201@gmail.com"
   def send_mail(inquiry)
     @inquiry = inquiry
     mail(
