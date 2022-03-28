78f5779d8666b4f8d106788e2a9d2ec1aa000089
Ryo-0912 <tensaisander0912@gmail.com>
2022-03-28 11:47:34 +0900

    メールに関する設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,8 +117,8 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     domain: 'gmail.com',
     port: 587,
-    g_mail: Rails.application.credentials.gmail[:g_mail],
-    g_pass: Rails.application.credentials.gmail[:g_pass],
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
     authentication: :login
   }
 
