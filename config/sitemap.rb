4d42bb7162e701aedeefd97ba6b653006f797161
Ryo-0912 <tensaisander0912@gmail.com>
2022-05-04 22:07:04 +0900

    sitemap.rbの5から8行目を元に戻す #84

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,10 +2,10 @@
 SitemapGenerator::Sitemap.default_host = 'https://www.mynort.com'
 SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/#{Rails.application.credentials.aws[:s3_bucket_name]}"
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
-  Rails.application.credentials.aws[:s3_bucket_name],
-  Rails.application.credentials.aws[:access_key_id],
-  Rails.application.credentials.aws[:secret_access_key],
-  Rails.application.credentials.aws[:default_region]
+  aws_s3_bucket_name: Rails.application.credentials.aws[:s3_bucket_name],
+  aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+  aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+  aws_default_region: Rails.application.credentials.aws[:default_region]
 )
 
 SitemapGenerator::Sitemap.create do
