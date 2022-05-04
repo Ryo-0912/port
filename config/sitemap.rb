25946ce7fff201ec630676dc1f7eed7052957184
Ryo-0912 <tensaisander0912@gmail.com>
2022-05-04 11:31:24 +0900

    sitemap.rbの編集 #76

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,11 +1,11 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = 'https://www.mynort.com'
-SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/#{ENV['Rails.application.credentials.aws[:s3_bucket_name]']}"
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/Rails.application.credentials.aws[:s3_bucket_name]"
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
-  aws_s3_bucket_name: ENV['Rails.application.credentials.aws[:s3_bucket_name]'],
-  aws_access_key_id: ENV['Rails.application.credentials.aws[:access_key_id]'],
-  aws_secret_access_key: ENV['Rails.application.credentials.aws[:secret_access_key]'],
-  aws_default_region: ENV['Rails.application.credentials.aws[:default_region]']
+  aws_s3_bucket_name: Rails.application.credentials.aws[:s3_bucket_name],
+  aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+  aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+  aws_default_region: Rails.application.credentials.aws[:default_region]
 )
 
 SitemapGenerator::Sitemap.create do
